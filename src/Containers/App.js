50947c165f73f27b6ff534c8f23c343c0a4bd430
António Sousa <antonio_carlos_alves_sousa@hotmail.com>
2019-09-13 15:53:48 +0100

    50% done

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 //import PropTypes from 'prop-types'
 
 function App () {
+
+  const [state, setState] = useState(false)
+
+  let _class = `skin-blue fixed sidebar-mini`
+
+  const buttonHandler = () => {
+    setState(prevState => ({ state: !prevState }))
+  }
+
+  if (state) {
+    _class = `login-page`
+  }
+
   return (
-    <>
+    <div className={_class} style={{ height: '100%' }}>
       <h1>Bank App</h1>
-    </>
+      <button onClick={() => buttonHandler()}>Click</button>
+    </div>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './Assets/Styles/index.css';
-import App from './Containers/App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './Assets/Styles/index.css'
+import App from './Containers/App'
+import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import 'bootstrap/dist/css/bootstrap.css'
+import 'admin-lte/dist/css/AdminLTE.min.css'
+import 'admin-lte/dist/css/skins/_all-skins.min.css'
+
+ReactDOM.render(<App/>, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
