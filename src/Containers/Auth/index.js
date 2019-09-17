ca22a5847c43d52f9af7aef2ff89ca3ef3cc5340
António Sousa <antonio_carlos_alves_sousa@hotmail.com>
2019-09-17 23:47:17 +0100

    login and signup completed

--- src/Containers/Auth/index.js
+++ src/Containers/Auth/index.js
@@ -7,12 +7,14 @@ import { useAuthStateValue } from '../../Store/Provider/auth'
 
 import LoginForm from '../../Components/Forms/login'
 import SignupForm from '../../Components/Forms/signup'
+import Loader from '../../Components/Spinner'
 
 import * as actionsTypes from '../../Store/Actions/Types'
 
 const AuthLoginPage = () => {
 
   const [page, setPage] = useState(false)
+  const [loading, setLoading] = useState(false)
   const [{ isAuth }, dispatch] = useAuthStateValue()
 
   const switchForm = e => {
@@ -25,6 +27,8 @@ const AuthLoginPage = () => {
   const submitHandler = ({ name, email, password, passwordConfirm }) => {
     let user = { name, email, password }
     let uri = `${openUriBankApi}/login`
+
+    setLoading(true)
 
     if (passwordConfirm) {
       user = { ...user, confirm_password: passwordConfirm }
@@ -45,44 +49,56 @@ const AuthLoginPage = () => {
           dispatch({ type: actionsTypes.AUTH_SUCCESS, payload })
         }
       }
-    ).catch(errors => console.log(errors))
+    ).catch(errors => {
+      setLoading(false)
+      // TODO: tratamento de erros
+      console.log(errors)
+    })
   }
 
-  let anchorText = 'Já tem conta? Entre aqui!'
-  let content = <SignupForm submitHandler={submitHandler}/>
+  let anchorText = 'Novo utilizador? Registe-se aqui!'
+  let authForm = <LoginForm submitHandler={submitHandler}/>
 
   if (page) {
-    anchorText = 'Novo utilizador? Registe-se aqui!'
-    content = <LoginForm submitHandler={submitHandler}/>
+    anchorText = 'Já tem conta? Entre aqui!'
+    authForm = <SignupForm submitHandler={submitHandler}/>
   }
+
+  let content = (
+    <div className="hold-transition login-page" style={{ height: '100%' }}>
+      <div className="wrapper">
+        <div className="login-box">
+          <div className="login-logo">
+            <b>Home</b> BANK
+          </div>
+          <div className="login-box-body">
+            <p className="login-box-msg">Bem vindo!</p>
+
+            {authForm}
+
+            <button
+              className="btn-link"
+              onClick={e => switchForm(e)}
+              style={{ padding: '10px 0 0 0', outline: 'none' }}>
+              {anchorText}
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 
   if (isAuth) {
     content = <Redirect to="/"/>
   }
 
+  if (loading) {
+    content = <Loader/>
+  }
+
   return (
     <React.Fragment>
-      <div className="hold-transition login-page" style={{ height: '100%' }}>
-        <div className="wrapper">
-          <div className="login-box">
-            <div className="login-logo">
-              <b>Home</b> BANK
-            </div>
-            <div className="login-box-body">
-              <p className="login-box-msg">Bem vindo!</p>
-
-              {content}
-
-              <button
-                className="btn-link"
-                onClick={e => switchForm(e)}
-                style={{ padding: '10px 0 0 0', outline: 'none' }}>
-                {anchorText}
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
+      {content}
     </React.Fragment>
   )
 }
